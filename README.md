6e65611574f433adbc3a4cf2b980d4e334490008
Benjamin Sternthal <ben@devpatch.com>
2014-02-22 12:19:45 -0800

    Refactoring.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ kanbender
 =========
 
 Kanbanery Helper For Moving Merged Cards To Done
+
+Note:
+
+Github setup instructions:
+# /kanbender/{project-name-in-json-file}
+
+// todo - fix the nconf stuff
+// todo - add kanbanery stuff
+// todo - possibly irc - not sure if needed
--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 'use strict';
 
+var nconf = require('nconf');
 var express = require('express');
 var app = express();
 
 var github = require('./lib/github');
+var bugzilla = require('./lib/bugzilla');
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.post('/kanbender', function(req, res) {
-    res.send('success');
+nconf.argv().env().file({ file: 'local.json' });
 
-    var bugs = github.getBugIDs(req.body.head_commit.message)
 
-    console.log(bugs);
+// for each bug mentioned we do this
+// then for each whiteboard tag we do the kanban thing
+// bugzilla.getKanbanId(948057, function(result) {
+//   console.log(result);
+// });
 
-    //Bugs Found Proceed
-    // if (bugs.length > 0) {
-    //     console.log(bugs);
-    // }
+// Main loop - project maps to settings file
+app.post('/kanbender/:project', function(req, res) {
+  console.log(req.params.project);
+  // res.send('success');
+
+  // var commits = req.body.commits;
+  // var bugs = github.getBugIDs(commits)
+
+  // // Bugs Found Proceed
+  // if (bugs.length > 0) {
+  //   //for each bug we need to look for the whiteboard tag in bugzilla
+  //   //call back hell!!
+
+  //}
 });
 
 app.listen(8000);
-
-//test me
